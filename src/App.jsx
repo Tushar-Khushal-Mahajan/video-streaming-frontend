df4d864991997df2afc51d3b3d1ebe712abbfd96
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2025-02-10 21:38:28 +0530

    CREATE REACT PROJECT WITH BASIC MODULES LIKE [AXIOS, FLOWBITE, TAILWIND-CSS]

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-center my-6 text-purple-600'>Hello World</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dicta corrupti aperiam alias blanditiis quas iure ipsum aliquam vitae amet, libero, quidem molestiae, qui nam nisi id commodi eligendi! Aliquam?
+        Veritatis sunt rem voluptates nisi laborum rerum itaque atque assumenda debitis corrupti! Sit facere debitis a nihil sed earum magni architecto, error, dolor totam omnis dolore fugit harum impedit eos.
+        Consequuntur optio vitae eos suscipit, enim quas, repellat maiores voluptates omnis architecto quae dignissimos sed nostrum iusto. Iure voluptates, aut porro eum culpa natus quia veritatis ad quo velit perspiciatis!
+        Laborum iure dicta cum veniam perspiciatis voluptas esse excepturi nesciunt, eaque nostrum quod voluptatibus qui. Nulla expedita nostrum, ratione quae maxime, quaerat inventore neque atque ea consectetur laboriosam eveniet aperiam?
+        Aspernatur tenetur deserunt minima necessitatibus laudantium magni dicta omnis inventore harum voluptatem nesciunt fugit ut perferendis in adipisci quos, ducimus, tempore dolorem labore sit, quod explicabo ullam voluptates fuga! Quisquam.
+        Error, porro inventore odio consequuntur repudiandae unde maxime omnis dolorum debitis nobis alias. Est nihil, repellat quod error consequuntur, hic minima rem corporis cupiditate eveniet vitae possimus delectus architecto quis.
+        Eos natus unde eius doloribus ipsam suscipit facilis? Impedit, atque fuga, quia quis esse dignissimos fugit illum architecto deleniti corrupti accusamus, enim repellat perferendis harum facere nulla modi. Eum, iure?
+        Iusto tempora velit id sunt ex expedita saepe minus corporis ut eos sequi a praesentium porro nulla sapiente perspiciatis temporibus esse deleniti, dolorem reiciendis! Distinctio sequi quaerat voluptatibus nisi nemo?
+        Iure recusandae quas provident repudiandae velit reiciendis corporis voluptas! Consequuntur adipisci nobis omnis quam. Qui ut architecto sunt soluta? Dignissimos suscipit sit harum, culpa blanditiis corporis velit aliquid quos facilis!
+        Dolore distinctio voluptatem labore adipisci rem similique a ea, repudiandae corporis, porro sunt culpa. Deleniti eaque reiciendis exercitationem. Repellendus odit culpa fuga consectetur unde soluta sequi ipsa tempora? Explicabo, repellendus.</p>
+    </>
+  );
+}
+
+export default App;
